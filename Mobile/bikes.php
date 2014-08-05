bf874abcf325db9f4d2a53ad5d39f87b904a8a6b
rdron <ranjandutta8@gmail.com>
2014-08-06 00:37:18 +0530

    update json file for bike listings

--- Mobile/bikes.php
+++ Mobile/bikes.php
@@ -1,5 +1,5 @@
 <?php
 $bike =array("6"=>"Hero Karizma","5"=>"Hero Kizma","4"=>"Heroarizma","2"=>"H Karizma","3"=>"Hero Kara","1"=>"Heo Karizma");
 
-echo "<html><body>".json_encode($bike)."</body></html>";
+echo json_encode($bike);
 ?>
